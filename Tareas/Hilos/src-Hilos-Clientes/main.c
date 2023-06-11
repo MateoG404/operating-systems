b805e0d4a758200624127bc1885ff42e17999d00
MateoG404 <mgutierrezca@unal.edu.co>
2023-06-10 19:24:25 -0500

    Definicion problema hilos con servidor local

---
+++ Tareas/Hilos/src-Hilos-Clientes/main.c
@@ -0,0 +1,2 @@
+// Crear un programa que se conecte a un servidor y este haga peticiones diferentes para la busqueda de un archivo 
+// El servidor debe crear diferentes hilos para poder atender a varios clientes
